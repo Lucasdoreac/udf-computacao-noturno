b0f8bb2b9c2c104dcb18f6d1a8c1f24a4f02bff3
Lucas Dórea Cardoso <160977944+Lucasdoreac@users.noreply.github.com>
2025-02-12 23:08:15 -0300

    Adiciona configuração do Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primary': '#1a56db',
+        'secondary': '#7c3aed',
+      },
+    },
+  },
+  plugins: [],
+}
